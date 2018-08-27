f48be7aaf31393a39cd6c1b3af71c6e2eebd66d7
Jim Gale <jimgaleforce@gmail.com>
2018-08-26 18:34:48 -0700

    a02a-added new USGSService (thread) task

---
+++ app/src/main/java/com/galeforce/quake1/USGSService.java
@@ -0,0 +1,12 @@
+package com.galeforce.quake1;
+
+import android.os.AsyncTask;
+
+public class USGSService extends AsyncTask<Void,Void,Void> {
+
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
